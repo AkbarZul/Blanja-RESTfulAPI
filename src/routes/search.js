261f1f24c46da76b62cf0012d89b9f210fae76b7
AkbarZul <akbar.zul73@gmail.com>
2020-11-24 18:34:35 +0700

    update search

--- src/routes/search.js
+++ src/routes/search.js
@@ -10,7 +10,7 @@ searchRouter.get("/", (req, res) => {
     const { q } = req.query;
     const key = "%" + q + "%"
     const searchProducts = new Promise((resolve, reject) => {
-        const querySearchProducts = "SELECT p.id, p.product_name, p.product_description, p.product_price, c.category_name, p.product_rating, p.product_size, p.product_color, p.product_total, p.product_condition, p.product_create, p.product_update FROM products AS p JOIN category AS c ON c.id = p.category_id WHERE category_name LIKE ?";
+        const querySearchProducts = `SELECT p.id, p.product_name, p.product_description, p.product_price, c.category_name, p.product_rating, p.product_size, p.product_color, p.product_total, p.product_condition, p.product_create, p.product_update FROM products AS p JOIN category AS c ON c.id = p.category_id WHERE category_name LIKE "%${key}" OR p.product_name LIKE "%${key}"`;
         db.query(querySearchProducts, key, (err, data) => {
             if (!err) {
                 resolve(data);
